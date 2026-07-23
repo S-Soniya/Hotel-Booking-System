663482680057d8644f676c30996cefc109880853
S-Soniya <soniyasubramanian27@gmail.com>
2026-07-23 17:28:57 +0530

    Add files via upload

---
+++ hotel - select.sql
@@ -0,0 +1,58 @@
+use hotelbookingsystem
+
+select * from hotel;
+
+select * from branch;
+
+select * from department;
+
+select * from room_type;
+
+select * from customer;
+
+select * from employee;
+
+select * from room;
+
+select * from service;
+
+select * from reservation;
+
+select * from checkin;
+
+select * from checkout;
+
+select * from payment;
+
+select * from invoice;
+
+select * from feedback;
+
+select * from service_booking;
+
+
+
+
+delete from service_booking;
+delete from feedback;
+delete from invoice;
+delete from payment;
+delete from checkout;
+delete from checkin;
+delete from reservation;
+
+delete from employee;
+
+delete from room;
+
+delete from service;
+
+delete from customer;
+
+delete from room_type;
+
+delete from department;
+
+delete from branch;
+
+delete from hotel;
